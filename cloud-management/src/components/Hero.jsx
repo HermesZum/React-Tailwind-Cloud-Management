f4330e5490ed70dc780ab22b9ff3cfdce20edafe
Clemente Nogueira <git.3gear@gmail.com>
2022-06-08 21:26:11 +0100

    The Hero component has been created.

---
+++ cloud-management/src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+} from "@heroicons/react/solid";
+
+import cyberBg from '../assets/img/cyber-bg.png';
+
+const Hero = () => {
+    return (
+        <section id="hero" className={'heroSection'}>
+            <div className={'heroContainer'}>
+                <div className={'heroDetails'}>
+                    <p className={'heroText'}>Unique Sequencing & Production</p>
+                    <h1 className={'heroTitle'}>Cloud Management</h1>
+                    <p className={'heroText'}>This is our Tech brand.</p>
+                    <button className={'heroButton'}>Get Started</button>
+                </div>
+                <div className={'heroImage'}>
+                    <img src={cyberBg} alt="cyber-bg" />
+                </div>
+                <div className={'heroServices'}>
+                    <h3>Data Services</h3>
+                    <div className={'heroServicesItems'}>
+                        <p><CloudUploadIcon className={'icon'}/>App Security</p>
+                        <p><DatabaseIcon className={'icon'}/>Dashboard Design</p>
+                        <p><PaperAirplaneIcon className={'icon'}/>Cloud Data</p>
+                        <p><ServerIcon className={'icon'}/>API</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
--- cloud-management/src/components/Navbar.jsx
+++ cloud-management/src/components/Navbar.jsx
@@ -1,15 +1,29 @@
+// Importing the React library and the useState hook from the React library.
 import React, {useState} from 'react';
+// Importing the MenuIcon and XIcon from the heroicons/react/outline package.
 import {MenuIcon, XIcon} from "@heroicons/react/outline";
 
+// This is a Navbar component.
 const Navbar = () => {
+    // A React hook that is used to set the state of the component.
     const [open, display] = useState(false);
+    // If the nav is open, close it. If the nav is closed, open it.
     const toggleNav = () => display( !open );
     return (
         <header>
+            {/**
+              * This is the navbar. It contains the brand, the navbar, the buttons, and the menu icon.
+            **/}
             <nav>
+                {/**
+                  * Creating a div that contains the brand name.
+                **/}
                 <div className={'brand'}>
                     <h1><a href="/">CLOUD.</a></h1>
                 </div>
+                {/**
+                  * Creating a list of links that are displayed on the navbar.
+                **/}
                 <div className={'navbar'}>
                     <ul>
                         <li><a href="/about">About</a></li>
@@ -19,14 +33,25 @@ const Navbar = () => {
                         <li><a href="/pricing">Pricing</a></li>
                     </ul>
                 </div>
+                {/**
+                  * Creating two buttons, one for logging in and one for creating an account.
+                **/}
                 <div className={'navbar'}>
                     <button>Login</button>
                     <button>Account</button>
                 </div>
+                {/**
+                  * Creating a div that contains a menu icon.
+                  * When the user clicks on the menu icon, the navbar is displayed.
+                **/}
                 <div className={'menuIcon'} onClick={toggleNav}>
                     {!open ? <MenuIcon/> : <XIcon />}
                 </div>
             </nav>
+            {/**
+              * This is the mobile navbar.
+              * It is hidden by default, but when the user clicks on the menu icon, it is displayed.
+            **/}
             <div className={!open ? 'hidden' : 'mobile'}>
                     <ul>
                         <li><a href="/about">About</a></li>
@@ -35,6 +60,9 @@ const Navbar = () => {
                         <li><a href="/platforms">Platforms</a></li>
                         <li><a href="/pricing">Pricing</a></li>
                     </ul>
+                {/**
+                  *Creating two buttons, one for logging in and one for creating an account.
+                **/}
                 <div className={'mobileButtons'}>
                     <button>Login</button>
                     <button>Account</button>
@@ -44,4 +72,5 @@ const Navbar = () => {
     );
 };
 
+// Exporting the Navbar component so that it can be imported into other files.
 export default Navbar;
