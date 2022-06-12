582af018e6321488793d678b8036e06db4e931ad
Clemente Nogueira <git.3gear@gmail.com>
2022-06-12 19:18:18 +0100

    The About component has been created.

---
+++ cloud-management/src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section className={'aboutSection'}>
+            <div className={'aboutContainer'}>
+                <div className={'aboutBody'}>
+                    <h2>Trusted by developers across the world</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Accusantium adipisci aliquid aperiam aspernatur assumenda deserunt distinctio est eveniet excepturi
+                        facere, illo impedit nihil odit provident quis suscipit, tempore veritatis voluptate!
+                    </p>
+                </div>
+                <div className={'aboutCards'}>
+                    <div className={'aboutCard'}>
+                        <h4>100%</h4>
+                        <p>Completion</p>
+                    </div>
+                    <div className={'aboutCard'}>
+                        <h4>24/7</h4>
+                        <p>Delivery</p>
+                    </div>
+                    <div className={'aboutCard'}>
+                        <h4>100K</h4>
+                        <p>Transactions</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default About;
