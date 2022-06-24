1180eb4e8bd4eaef3f39e448ada55e5809e89c2d
Clemente Nogueira <git.3gear@gmail.com>
2022-06-24 22:30:57 +0100

    The AllInOne component has been created.

--- cloud-management/src/App.js
+++ cloud-management/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Support from "./components/Support";
+import AllInOne from "./components/AllInOne";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Hero/>
           <About/>
           <Support/>
+          <AllInOne/>
       </>
   );
 }
---
+++ cloud-management/src/components/AllInOne.jsx
@@ -0,0 +1,131 @@
+/* Importing the React library from the node_modules folder. */
+import React from 'react';
+/* Importing the CheckIcon from the heroicons package. */
+import { CheckIcon } from "@heroicons/react/outline";
+
+/**
+ * It returns the AllInOne component.
+ * @returns AllInOne component.
+ */
+const AllInOne = () => {
+    return (
+        <section className={ 'allInOneContainer' }>
+            <div className={ 'allInOneHeaderContainer' }>
+                <h2>All-in-One Platform</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, corporis dolor eius eum labore
+                    molestias nemo officiis pariatur possimus voluptates.
+                </p>
+                <div className={ 'allInOneCards' }>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={ 'allInOneCard' }>
+                        <div>
+                            <CheckIcon className={ 'allInOneCardIcon' }/>
+                        </div>
+                        <div>
+                            <h3>Notifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aperiam beatae
+                                blanditiis, deleniti ipsa nam odio porro rem totam voluptate!
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
+};
+/* Exporting the component to be used in other files. */
+export default AllInOne;
